848d1b0edc96b11c538c4c06fe5e3e271e11b5de
deejayjay15 <jaxomwangi@gmail.com>
2022-09-22 13:33:51 +0000

    ssssggg

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative checks whether i is positive or negative
+ */
+void positive_or_negative(int i);
+
+/**
+ *int largest_number returns the largest number of three
+ */
+int largest_number(int a, int b, int c);
+
+/**
+ *int convert_day converts day of the month to day of the year
+ */
+int convert_day(int month, int day);
+
+/**
+ *print_remaining_days prints rmeining days of the year
+ */
+void print_remaining_days(int month, int day, int year);
+#endif /* MAIN_H */
