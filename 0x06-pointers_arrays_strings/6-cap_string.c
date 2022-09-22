97c81f27f6ff8e169a48feb23dea16b79bea4bd4
deejayjay15 <jaxomwangi@gmail.com>
2022-09-22 19:02:36 +0000

    weeweew

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -31,37 +31,4 @@ char *cap_string(char *s)
 		i++;
 	}
 	return (s);
-}#include "main.h"
- /**
-  * cap_string - converts first byte of a string to uppercase
-  *
-  * @s: string to be converted
-  *
-  * Return: c(The string converted to uppercase)
-  *
-  */
-
-char *cap_string(char *s)
-{
-	int i;/*Iterator*/
-
-	i = 0;
-
-	if (s[i] <= 'z' && s[i] >= 'a')
-		s[i] = s[i] - 'a' + 'A';
-
-	i++;
-
-	while (s[i] != '\0')
-	{
-		if ((s[i] >= 'a' && s[i] <= 'z') &&
-		    (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
-		          s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-		     s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-		     s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
-		     || s[i - 1] == '\n'))
-			s[i] = s[i] - 'a' + 'A';
-		i++;
-	}
-	return (s);
 }
